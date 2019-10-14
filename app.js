4b3169cc5fd41ca1bdd0fb75e020457bee25a01e
Opilane TTHK <opilane@tthk.ee>
2019-10-14 09:02:04 +0300

    Upload

--- app.js
+++ app.js
@@ -1,15 +1,39 @@
-loadEventListeners();
+const github = new GitHub;
+const ui = new UI;
+const searchUser = document.getElementById('searchUser');
+searchUser.addEventListener('keyup', (event) => {
+    const userText = event.target.value;
+    if(userText !== '') {
+        github.getUser(userText)
+        .then(data => {
+            if(data.profile.message === 'Not Found') {
+                ui.showAlert('User not found!', 'alert alert-danger')
+            } else {
+                ui.showProfile(data.profile);
+                ui.showRepos(data.repos);
+            }
+            console.log(data);
+        })
+    } else {
+        ui.clearProfile();
+    }
+})
 
-function loadEventListeners(){
+/*
+loadEventListener();
+
+function loadEventListener() {
     document.addEventListener('DOMContentLoaded', getObject);
 }
 
 function getObject() {
-    const gitHub=new Github;
-    console.log(gitHub);
+    const gitHub = new GitHub;
+    const ui = new UI;
 
-    gitHub.getUser('Nikoljka')
-    .then (data=> {
+    gitHub.getUser('t3rkecorejz').then(data => {
         console.log(data);
-    })
+        //ui.showProfile(data.profile);
+        //ui.showProfile(data.repos);
+    });
 }
+*/
--- github.js
+++ github.js
@@ -1,21 +1,19 @@
-class Github {
+class GitHub {
     constructor() {
-        this.client_id='f7f5341cc6f569070681';
-        this.client_secret='6eef5e2671f0f2f59ac2c30ad0364b21c4e5e812';
-        this.repos_count= 5;
-        this.repos_sort='created: asc';
+        this.client_id = '017fa67e6d7822aa8acb';
+        this.client_secret = '266effa47b98ff3ff7e7c2a45e49e3a93c2ef361';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
     }
-    async getUser(user){
-        const profileResponse=await fetch(`https://api.github.com/users/${user}?cliend_id=${this.client_id}&client_secret=${this.client_secret}`);
-        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_cpunt}$sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret} `);
-        const profile=await profileResponse.json();
-        const repos= await repoResponse.json();
 
+    async getUser(user) {
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+        const reposResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+        const profile = await profileResponse.json();
+        const repos = await reposResponse.json();
 
-        return{
-            profiole, repos
+        return {
+            profile, repos
         }
-
     }
-
 }
